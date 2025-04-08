c76393ec1634e3e9e7c4e2a3258d922dd47c650f
nhuynh011 <114946408+nhuynh011@users.noreply.github.com>
2025-04-08 11:39:23 -0400

    Updated ZAGI DW ETL 3 Multiple Sources.sql

--- ZAGI DW ETL 3 Multiple Sources.sql
+++ ZAGI DW ETL 3 Multiple Sources.sql
@@ -1,22 +1,27 @@
 -- ETL for Products category
+
+-- Loading sales product
+-- Select sales products
 SELECT p.productid, p.productname, p.productprice, p.vendorid, p.categoryid, c.categoryname, v.vendorname
 FROM mihuynh_ZAGImore.product p, mihuynh_ZAGImore.category c, mihuynh_ZAGImore.vendor v
 WHERE p.vendorid = v.vendorid AND p.categoryid = c.categoryid
 
 -- insert into, make sure that the order is the same
-INSERT INTO product_dim(ProductID, ProductName, SalesProductPrice, VendorID, VendorName, CategoryID, CategoryName, ProductType)
-SELECT p.productid, p.productname, p.productprice, p.vendorid, p.categoryid, c.categoryname, v.vendorname, "SalesProduct"
+INSERT INTO Product_Dimension(ProductID, ProductName, SalesProductPrice, VendorID, VendorName, CategoryID, CategoryName, ProductType, ExtractionTimestamp, pd_loadstatus)
+SELECT p.productid, p.productname, p.productprice, p.vendorid, v.vendorname, p.categoryid, c.categoryname, "SalesProduct", NOW(), FALSE
 FROM mihuynh_ZAGImore.product p, mihuynh_ZAGImore.category c, mihuynh_ZAGImore.vendor v
-WHERE p.vendorid = v.vendorid AND p.categoryid = c.categoryid
+WHERE p.vendorid = v.vendorid AND p.categoryid = c.categoryid;
 
+
+-- Loading rental product
 -- make sure you have the right variable type set for these columns ^^
 -- this is only sales product, we have to add rental products into this as well.
-SELECT r.productid, r.productname, r.productpricedaily, r.productpriceweekly, r.vendorid, r.categoryid, c.categoryname, v.vendorname, "RentalProduct"
+SELECT r.productid, r.productname, r.productpricedaily, r.productpriceweekly, r.vendorid, v.vendorname, r.categoryid, c.categoryname,  "RentalProduct", NOW(), FALSE
 FROM mihuynh_ZAGImore.rentalProducts r, mihuynh_ZAGImore.category c, mihuynh_ZAGImore.vendor v
-WHERE r.vendorid = v.vendorid AND r.categoryid = c.categoryid
+WHERE r.vendorid = v.vendorid AND r.categoryid = c.categoryid;
 
 -- Now we should be able to insert into:
-INSERT INTO product_dim(ProductID, ProductName, RentalProductPriceDaily, RentalProductPriceWeekly, VendorID, VendorName, CategoryID, CategoryName, ProductType)
-SELECT r.productid, r.productname, r.productpricedaily, r.productpriceweekly, r.vendorid, r.categoryid, c.categoryname, v.vendorname, "RentalProduct"
+INSERT INTO Product_Dimension(ProductID, ProductName, RentalProductPriceDaily, RentalProductPriceWeekly, VendorID, VendorName, CategoryID, CategoryName, ProductType, ExtractionTimestamp, pd_loadstatus)
+SELECT r.productid, r.productname, r.productpricedaily, r.productpriceweekly, r.vendorid, v.vendorname, r.categoryid, c.categoryname,  "RentalProduct", NOW(), FALSE
 FROM mihuynh_ZAGImore.rentalProducts r, mihuynh_ZAGImore.category c, mihuynh_ZAGImore.vendor v
-WHERE r.vendorid = v.vendorid AND r.categoryid = c.categoryid
+WHERE r.vendorid = v.vendorid AND r.categoryid = c.categoryid;
